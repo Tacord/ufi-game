e17d7e35c9aa3a09180d1e18139dae019d88a82b
Kalio <83121102+Tacord@users.noreply.github.com>
2025-11-18 15:21:06 +0800

    Ringpull magnetism

--- scripts/magnet.gd
+++ scripts/magnet.gd
@@ -2,6 +2,8 @@ extends Area2D
 var canAttack : bool = true
 var SPEED = 0
 var lerpSpeed : float = 100
+var currentDelta : float = 0
+
 @onready var player = $"../Player"
 
 # Called when the node enters the scene tree for the first time.
@@ -11,11 +13,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	currentDelta = delta
 	if Input.is_action_just_pressed("z") and canAttack:
 		canAttack = false
 		SPEED = -2700
 	position.y += SPEED * delta
-		
+
+
 	lerpSpeed = 2000 * 1/(player.position.y - position.y)
 	if position.y < 800:
 		SPEED += 6000 * delta
@@ -27,3 +31,7 @@ func _process(delta: float) -> void:
 	var original_y = position.y
 	position = position.lerp(player.position, lerpSpeed * delta)
 	position.y = original_y
+
+func _on_area_entered(area):
+	if area.is_in_group("ringpull"):
+		area.queue_free()
--- scripts/ringpull.gd
+++ scripts/ringpull.gd
@@ -2,6 +2,8 @@ extends Area2D
 var rng = RandomNumberGenerator.new()
 var SPEED = 500
 var ROTATIONVAL = 2
+@onready var player = $"../Player"
+@onready var magnet = $"../Magnet"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,10 +12,13 @@ func _ready() -> void:
 	ROTATIONVAL = rng.randf_range(-3,3)
 	scale.x = rng.randf_range(0.15,0.25)
 	scale.y = scale.x
-	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$Sprite2D.rotation += ROTATIONVAL * delta
 	position.y += SPEED * delta
+	
+	if abs(global_position.x - magnet.global_position.x) < 500 and abs(global_position.y - magnet.global_position.y) < 400:
+		var direction: Vector2 = global_position.direction_to(magnet.global_position)
+		var movement_amount: Vector2 = direction * 500 * delta
+		global_position += movement_amount
